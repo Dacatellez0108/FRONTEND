775db4c2ccead27b05135bcf8507fc987fac4e35
Daniel.Tellez08 <dacate0203@gmail.com>
2024-11-03 23:28:49 -0500

    commit final

--- js/Productos.js
+++ js/Productos.js
@@ -1,6 +1,6 @@
-function mostrarProveedores() {
-    let request = sendRequest('Proveedores', 'GET', '');
-    let table = document.getElementById('Proveedores-table');
+function mostrarProductos() {
+    let request = sendRequest('Productos', 'GET', '');
+    let table = document.getElementById('Productos-table');
     table.innerHTML = "";
     request.onload = function () {
         let data = request.response;
@@ -8,14 +8,14 @@ function mostrarProveedores() {
         data.forEach(element => {
             table.innerHTML += `
         <tr class="table-dark">
-            <td>${element.nombres}</td>
-            <td>${element.ciudad}</td>
-            <td>${element.documento}</td>
-            <td>${element.correo}</td>
-            <td>${element.telefono}</td>
-            <td>${element.direccion}</td>
+            <td>${element.nombre}</td>
+            <td>${element.cantidad}</td>
+            <td>${element.precio}</td>
+            <td>${element.descripcion}</td>
+            <td>${element.categoria}</td>
+           
             <td>
-                <button type="button" class="btn btn-primary w-auto" onclick="window.location ='/formProveedores.html?id=${element._id}'">Editar</button>
+                <button type="button" class="btn btn-primary w-auto" onclick="window.location ='/formProductos.html?id=${element._id}'">Editar</button>
                 <button type="button" class="btn btn-danger w-auto" onclick='deleteClientes("${element._id}")'>Eliminar</button>
             </td>
         </tr>
